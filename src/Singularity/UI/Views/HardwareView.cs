e61f6a79c32db536140aff155ff998bb4f790cd7
Dave Beusing <david.beusing@gmail.com>
2026-09-22 23:43:36 +0200

    refactor(ui): split qualification results and reports views

--- src/Singularity/UI/Views/HardwareView.cs
+++ src/Singularity/UI/Views/HardwareView.cs
@@ -2,8 +2,8 @@
 // Licensed under the MIT License.
 // See LICENSE file in the project root for full license information.
 
-using Singularity.Hardware.Providers;
 using Singularity.Hardware.Models;
+using Singularity.Hardware.Providers;
 using Singularity.UI.Layout;
 using Singularity.UI.Sections;
 
@@ -12,6 +12,7 @@ namespace Singularity.UI.Views;
 public sealed class HardwareView : Panel
 {
 	private readonly HardwareProvider hardwareProvider = new();
+
 	public HardwareInventory Inventory { get; private set; } = new();
 
 	public HardwareView()
@@ -21,32 +22,39 @@ public sealed class HardwareView : Panel
 		Width = LayoutConstants.MainWidth;
 		BackColor = Theme.Background;
 
-		BuildUi();
+		RefreshInventory();
 	}
 
-	private void BuildUi()
+	public void RefreshInventory()
 	{
-		Controls.Clear();
-
-		Inventory = hardwareProvider.Read();
-
-		OsSection osSection = new(Inventory.Os)
+		SuspendLayout();
+		try
 		{
-			Left = 0,
-			Top = 0
-		};
+			Controls.Clear();
+			Inventory = hardwareProvider.Read();
 
-		HardwareSection hardwareSection = new(Inventory)
+			OsSection osSection = new(Inventory.Os)
+			{
+				Left = 0,
+				Top = 0
+			};
+
+			HardwareSection hardwareSection = new(Inventory)
+			{
+				Left = 0,
+				Top = osSection.Bottom + LayoutConstants.SectionGap
+			};
+
+			Controls.AddRange([
+				osSection,
+				hardwareSection
+			]);
+
+			Height = hardwareSection.Bottom;
+		}
+		finally
 		{
-			Left = 0,
-			Top = osSection.Bottom + LayoutConstants.SectionGap
-		};
-
-		Controls.AddRange([
-			osSection,
-			hardwareSection
-		]);
-
-		Height = hardwareSection.Bottom;
+			ResumeLayout(true);
+		}
 	}
 }
